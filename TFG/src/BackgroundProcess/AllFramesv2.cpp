c6ad49ba9221d56d9def5241cc169f8c44bc4b44
Said Atrahouch Echarrouti <said.atrahouch@gmail.com>
2016-04-02 13:25:30 +0200

    Static Background Model comment

--- TFG/src/BackgroundProcess/AllFramesv2.cpp
+++ TFG/src/BackgroundProcess/AllFramesv2.cpp
@@ -8,27 +8,17 @@
 #include "AllFramesv2.h"
 
 AllFramesv2::AllFramesv2() {
-	// TODO Auto-generated constructor stub
-
 }
 
 AllFramesv2::~AllFramesv2() {
-	// TODO Auto-generated destructor stub
 }
 
 /**
- * La técnica que ha sido aplicada para obtener la máscara del plano ha sido el modelo del fondo estático
- *
- * Este proceso se basa en la siguiente fórmula matemática:
- * <<Insertar después en Latex>>
- *
  * En el estado inicial, el algoritmo no tiene guardado el primer fotograma del vídeo. Al carecer del
  * primer fotograma, este se guarda y se devuelve una máscara negra, si se calculase se obtendría eso.
  *
- * En los estados consecutivos, se va comparando con el frame guardado y obtenemos la máscara asociada.
- *
- * Esta técnica tiene un problema que es el `ghosting`. El problema aparece cuando el primer fotograma hay
- * un objeto en movimiento, las máscarás consecutivas obtenidas se formará la sombra del objeto en movimiento.
+ * En los estados consecutivos, se va comparando con el frame guardado y obtenemos la máscara de
+ * primer plano asociada.
  *
  * @param input Fotograma, del vídeo, que se va a manipular
  * @param output Máscara del primer plano obtenido tras manipular el fotograma de entrada
