13367d931163d14c90f4ec5ffe001d0dd186c5b5
Said Atrahouch Echarrouti <said.atrahouch@gmail.com>
2016-04-02 15:21:38 +0200

    Fixed Comment Static Background Model

--- TFG/src/BackgroundProcess/FondoEstatico.cpp
+++ TFG/src/BackgroundProcess/FondoEstatico.cpp
@@ -16,6 +16,16 @@ FondoEstatico::~FondoEstatico() {
 	// TODO Auto-generated destructor stub
 }
 
+/**
+ * En el estado inicial, el algoritmo no tiene guardado el primer fotograma del vídeo. Al carecer del
+ * primer fotograma, este se guarda y se devuelve una máscara negra, si se calculase se obtendría eso.
+ *
+ * En los estados consecutivos, se va comparando con el frame guardado y obtenemos la máscara de
+ * primer plano asociada.
+ *
+ * @param input Fotograma, del vídeo, que se va a manipular
+ * @param output Máscara del primer plano obtenido tras manipular el fotograma de entrada
+ */
 void FondoEstatico::process(cv::Mat& input, cv::Mat& output){
 	if(output.empty()){
 		output = cv::Mat::zeros(input.rows, input.cols, CV_8UC1);
--- TFG/src/BackgroundProcess/FondoEstatico.h
+++ TFG/src/BackgroundProcess/FondoEstatico.h
@@ -11,7 +11,25 @@
 #include <iostream>
 #include "FrameProcessor.h"
 
-
+/**
+ * El modelo de susbtración implementado en esta clase es el modelo de Fondo estático.
+ *
+ * Este proceso se basa en la siguiente fórmula matemática:
+ * <<Insertar después en Latex>>
+ *
+ * El modelo se basa en la comparación continua con el primer frame del vídeo. Este modelo
+ * computacionalmente es bajo pero sufre de carencias en entornos donde el inicio del vídeo contiene
+ * objetos que están en movimiento.
+ *
+ * El problema previamente mencionado se le llama `ghosting`, que consiste que el objeto
+ * en movimiento, capturado en el primer frame, siempre se mostrará su sombra, o estela,
+ * a no ser que el objeto vuelva a su posición inicial.
+ *
+ * Este modelo es dependiente a (Forma lista) :
+ * La imagen de fondo tomada: si hay un objeto en movimiento en ese frame, se formará su sombra
+ * La eleción del umbral: Un valor muy alto genera falsos-negativos pero uno muy bajo genera falsos-positivos
+ * Las condiciones de la escena que se esta tratando.
+ */
 class FondoEstatico: public FrameProcessor {
 public:
 	FondoEstatico();
